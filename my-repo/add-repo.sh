c25b73ba8ec052fcc32ab1f8ca7466269435a016
Itai Nelken <70802936+Itai-Nelken@users.noreply.github.com>
2021-02-11 16:49:20 +0200

    Create add-repo.sh & Update HEADER.html

---
+++ my-repo/add-repo.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#check that script is being run as root
+if [ ! "$EUID" = 0 ]; then
+    echo "The script wasn't run as root!"
+    echo "you NEED to run this script as root!"
+    exit 1
+fi
+
+# flags. default is to update with extra output and ask to exit.
+if [[ "$1" == "--version" ]] || [[ "$1" == "-v" ]]; then
+  echo -e "
+  $(tput setaf 9)###############################################
+  $(tput setaf 9)#$(tput setaf 4)$(tput bold)add-repo.sh version 1.0 by Itai-Nelken | 2021$(tput sgr 0)$(tput setaf 9)#
+  $(tput setaf 9)###############################################$(tput sgr 0)
+  "
+  exit 0
+elif [[ "$1" == "--help" ]] || [[ "$1" == "-h" ]]; then
+  echo -e "
+    $(tput setaf 6)$(tput bold)USAGE:$(tput sgr 0)
+    $(tput bold)intended usage:$(tput sgr 0)
+    wget -qO- https://itai-nelken.github.io/my-repo/add-repo.sh | sudo bash
+
+    $(tput setaf 6)$(tput bold)Extra commands that have no use:$(tput sgr 0)
+    --version or -v - print version and exit.
+    --help or -h - print this help and exit.
+  "
+  exit 0
+fi
+
+echo -e "$(tput setaf 6)adding repo...$(tput sgr 0)"
+echo ' + echo "deb [trusted=yes] https://itai-nelken.github.io/my-repo/ /" | tee -a /etc/apt/sources.list > /dev/null'
+echo "deb [trusted=yes] https://itai-nelken.github.io/my-repo/ /" | tee -a /etc/apt/sources.list > /dev/null
+echo -e "$(tput setaf 6)running apt update...$(tput sgr 0)"
+echo " + sudo apt update"
+sudo apt update
+echo -e "$(tput setaf 2)DONE!$(tput sgr 0)"
+echo -e "$(tput setaf 6)$(tput bold)ALL APPS$(tput sgr 0)"
+echo " - hello-world"
+echo "   * print hello world on the screen, written in c."
+echo " - count100"
+echo "   * count to 100 and back and print the output on the screen, written in c."
+echo " - qemu2deb"
+echo "   * compile, install, and package into a deb the latest version of QEMU, written in bash."
+echo " - pi-temp"
+echo "   * print current the Raspberry Pi's CPU temperature, updates in 1 second intervals. written in bash."
+echo " - cpu-clock"
+echo "   * print current the Raspberry Pi's CPU clock speed, updates in 1 second intervals. written in bash."
