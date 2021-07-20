ab3fad11f92692aafec0e999db84f7b7e656dcce
Itai-Nelken <itainelken@gmail.com>
2021-07-20 15:15:42 +0300

    Update stuff/manage.sh
    Signed-off-by: Itai-Nelken <itainelken@gmail.com>

--- stuff/manage.sh
+++ stuff/manage.sh
@@ -3,9 +3,13 @@
 # current working directory
 DIR="$(pwd)"
 # repositories to ignore when 'git pull'ing
-ignore_list=('')
+ignore_list=()
 # list of all repo names in DIR
-repo_names=('')
+repo_names=()
+# hold amount of errors
+errnum=0
+# hold errors
+errors=()
 
 function help() {
 	echo -e "\e[1mUSAGE:\e[0m ./manage.sh [command] [option/command]"
@@ -14,6 +18,11 @@ function help() {
 	echo -e "\e[1mpush\e[0m - git push all the repos in the current folder.\n    ignore repos by adding the '--ignore' flag followed by a list of the folder names of the repos to ignore."
 }
 
+function increment() {
+	result=$1
+	result=$((result+1))
+	return $result
+}
 
 # get all repo names in DIR and store them in the repo_names array
 function get-repo-names() {
@@ -28,7 +37,7 @@ function get-repo-names() {
 }
 
 if [[ "$1" != "" ]]; then
-	case $1 in
+	case $1 in 
 		update|pull)
 			if [[ ! -z "$2" ]] && [[ "$2" != "--ignore"* ]]; then
 				if [[ -d "$2" ]]; then
@@ -49,13 +58,11 @@ if [[ "$1" != "" ]]; then
 			fi
 			get-repo-names
 			for repo in ${repo_names[@]}; do
-				if [[ ! "${repo}" =~ "${ignore_list[@]}" ]] || [[ "${ignore_list[@]}" == '' ]]; then
-					if [[ -d "${repo}" ]]; then
-						echo -e "\e[1m$repo\e[0m"
-						cd "$repo"
-						git pull
-						cd "$DIR"
-					fi
+				if [[ ! "${repo_names[@]}" =~ "${ignore_list[@]}" ]] || [[ "${ignore_list[@]}" == '' ]]; then
+					echo -e "\e[1m$repo\e[0m"
+					cd "$repo"
+					git pull
+					cd "$DIR"
 				fi
 			done
 		;;
@@ -65,9 +72,9 @@ if [[ "$1" != "" ]]; then
 					echo -e "\e[1m$2\e[0m"
 					cd "$2"
 					git push
-					pid=$(echo $?)
+					exit=$(echo $?)
 					cd "$DIR"
-					exit $pid
+					exit $exit
 				else
 					echo -e "\e[1;31mERROR:\e[0;31m There is no repo called '$2' locally!\e[0m"
 					exit 1
@@ -79,12 +86,22 @@ if [[ "$1" != "" ]]; then
 			fi
 			get-repo-names
 			for repo in ${repo_names[@]}; do
-				if [[ ! "${repo}" =~ "${ignore_list[@]}" ]] || [[ "${ignore_list[@]}" == '' ]]; then
+				if [[ ! "${repo_names[@]}" =~ "${ignore_list[@]}" ]] || [[ "${ignore_list[@]}" == '' ]]; then
 					echo -e "\e[1m$repo\e[0m"
 					cd "$repo"
 					git push
+					if [[ $? != 0 ]]; then
+						errnum=$((errnum+1))
+						errors[$errnum]="$repo"
+					fi
 					cd "$DIR"
 				fi
+			done
+			echo -e "\e[1;31m$errnum errors happened in repositories:\e[0m"
+			i=1
+			for repo in ${errors[@]}; do
+				echo "$i) $repo"
+				increment $i
 			done
 		;;
 		help|-h|--help)
